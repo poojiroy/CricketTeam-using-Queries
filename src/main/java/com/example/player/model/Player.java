15b8a49256f599bc4c44a96e9c9e1091c4299700
poojiroy <100796381+poojiroy@users.noreply.github.com>
2023-03-13 02:00:10 +0530

    Create Player.java

---
+++ src/main/java/com/example/player/model/Player.java
@@ -0,0 +1,47 @@
+// Write your code here
+package com.example.player.model;
+import javax.persistence.*;
+@Entity
+@Table(name="player")
+public class Player{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)  
+@Column(name="playerId")
+    private int playerId;
+    @Column(name="playerName")
+    private String playerName;
+    @Column(name="jerseyNumber")
+    private int jerseyNumber;
+    @Column(name="role")
+    private String role;
+    public Player(int playerId, String playerName, int jerseyNumber, String role){
+        this.playerId=playerId;
+        this.playerName=playerName;
+        this.jerseyNumber=jerseyNumber;
+        this.role=role;
+    }
+    public void setPlayerId(int playerId){
+        this.playerId=playerId;
+    }
+    public int getPlayerId(){
+        return playerId;
+    }
+    public void setPlayerName(String playerName){
+        this.playerName=playerName;
+    }
+    public String getPlayerName(){
+        return playerName;
+    }
+    public void setJerseyNumber(int jerseyNumber){
+        this.jerseyNumber=jerseyNumber;
+    }
+    public int getJerseyNumber(){
+        return jerseyNumber;
+    }
+    public void setRole(String role){
+        this.role=role;
+    }
+    public String getRole(){
+        return role;
+    }
+}
